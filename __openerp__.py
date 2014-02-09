3cba4c7e3789b52d08f238ce381433c4f0512e6e
Carlos Paredes <cparedes@covetel.com.ve>
2014-02-08 19:57:26 -0430

    Inicializando módulo
    Signed-off-by: Carlos Paredes <cparedes@covetel.com.ve>

---
+++ __openerp__.py
@@ -0,0 +1,16 @@
+{
+   'name': 'PostScripts reports',
+   'version': '0.01',
+   'category': 'Reports',
+   'description': "Openerp module that adds a postscript reports certain existing modules",
+   'author': 'Covetel R.S',
+   'website': 'http://www.covetel.com.ve',
+   'depends': ['stock', 'procurement', 'board', 'sale'],
+   'data': [],
+   'init_xml': [],
+   'update_xml': [],
+   'demo_xml': [],
+   'test': [],
+   'installable': True,
+   'active': False,
+}
