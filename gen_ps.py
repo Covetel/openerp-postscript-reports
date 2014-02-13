94105f2b50c156e6781b4453161014f21549a793
Carlos Paredes <cparedes@covetel.com.ve>
2014-02-13 14:50:27 -0430

    Corrigiendo formato del postscript
    Signed-off-by: Carlos Paredes <cparedes@covetel.com.ve>

--- gen_ps.py
+++ gen_ps.py
@@ -10,8 +10,11 @@ invoice.update({'rif' : '',
 
 table_items = ""
 
+table_products_y_pos = 15
+tables_x_pos = 1.1
+
 for line in invoice['invoice_lines']:
-    table_items += r""""""+str(line['invoice_line_quantity'])+r"""&"""+line['invoice_line_name']+r"""&"""+str(line['invoice_line_price_unit'])+r"""&"""+str(line['invoice_line_price_subtotal'])+r"""\\"""
+    table_items += r"""\centering"""+str(line['invoice_line_quantity'])+r"""&"""+line['invoice_line_name']+r"""& \centering"""+str(line['invoice_line_price_unit'])+r"""&"""+str(line['invoice_line_price_subtotal'])+r"""\\"""
 
 def generate_postscript(invoice_dict):
     text.set(mode="latex")
@@ -30,42 +33,44 @@ def generate_postscript(invoice_dict):
     c.text(12, 17.5, "Contribuyente No Sujeto.")
 
     #Cuadro partner_name o razon social
-    c.text(1.1, 16.9, r"""\begin{tabular*}{1.61\columnwidth}{@{\extracolsep{\fill}}|c|c|}
+    c.text(tables_x_pos, 16.9, r"""\begin{tabular*}{1.61\columnwidth}{|p{14cm}|p{4.65cm}|}
       \hline
       \textbf{NOMBRE O RAZON SOCIAL:} """+invoice_dict['partner_name']+r"""& \textbf{RIF:} """+invoice_dict['rif']+r""" \\
       \hline
     \end{tabular*}""")
 
-    c.text(1.1, 16.46, r"""\begin{tabular}{ | c | }
+    c.text(tables_x_pos, 16.46, r"""\begin{tabular*}{1.61\columnwidth}{|p{19.07cm}|}
       \hline
       \textbf{FORMA DE PAGO:} CONTADO \\
       \hline
-    \end{tabular}""")
+    \end{tabular*}""")
 
     #Cuadro forma de pago
     #c.stroke(path.rect(1, 16.3, 19, 0.5))
 
     #Header tabla productos
-    c.text(1, 15, r"""\begin{tabular*}{1.61\columnwidth}{@{\extracolsep{\fill}}|c|c|c|c|}
+    c.text(tables_x_pos, table_products_y_pos, r"""\begin{tabular*}{1.61\columnwidth}{|p{2.3cm}|p{11cm}|p{2.3cm}|p{2.22cm}|}
       \hline
-      \textbf{CANTIDAD} & \textbf{CONCEPTO O DESCRIPCION} & \textbf{PRECIO UNITARIO} & \textbf{VALOR TOTAL} \\
+      \centering\textbf{CANTIDAD} & \centering\textbf{CONCEPTO O DESCRIPCION} & \centering\textbf{PRECIO UNITARIO} & \textbf{VALOR TOTAL} \\
       \hline
       """+table_items+r"""
       \hline
     \end{tabular*}""")
 
     #Total
-    c.text(15.6, 13, r"\textbf{Subtotal Gravado Bs.F:} "+str(invoice_dict['invoice_amount_untaxed']))
-    c.text(17, 12.5, r"\textbf{IVA 0\% Bs.F:} 0.00")
-    c.text(16.2, 12, r"\textbf{Total a Pagar Bs.F:} "+str(invoice_dict['invoice_amount_total']))
+    c.text(15.3, (table_products_y_pos - 2), r"""\begin{tabular}{r}
+    \textbf{Subtotal Gravado Bs.F:} """+str(invoice_dict['invoice_amount_untaxed'])+r""" \\ """+
+    r"""\textbf{IVA 0\% Bs.F:} 0.00 \\"""+
+    r"""\textbf{Total a Pagar Bs.F:} """+str(invoice_dict['invoice_amount_total'])+r"""\\"""+
+    r"""\end{tabular}""")
 
     #Cuadro invoice_footer (cuenta corriente)
-    c.text(1, 1.2, r"""\begin{tabular}{ | c |}
+    c.text(tables_x_pos, 1.2, r"""\begin{tabular*}{19.5cm}{|p{19.1cm}|}
       \hline
       """+invoice_dict['invoice_footer1']+r""" \\
       """+invoice_dict['invoice_footer2']+r""" \\
       \hline
-    \end{tabular}""")
+    \end{tabular*}""")
 
     #page = document.paperformat(8.5 inch, 11 inch, name=A0)
     page = document.page(c, paperformat="Letter", centered=0)
