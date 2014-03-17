f3ba5f1f41c0dca1973ad7bf08ca88608cb1ae50
Carlos Paredes <cparedes@covetel.com.ve>
2014-03-17 11:17:52 -0430

    Corrigiendo error al obtener los datos de la factura en el js de prueba para la impresión de facturas con impresora fiscal
    Signed-off-by: Carlos Paredes <cparedes@covetel.com.ve>

--- static/src/js/print_invoices.js
+++ static/src/js/print_invoices.js
@@ -1,7 +1,8 @@
 openerp.plan_cuentas_cooperativas = function (instance) {
     instance.web.client_actions.add('printer.action', 'instance.plan_cuentas_cooperativas.action');
     instance.plan_cuentas_cooperativas.action = function (parent, action) {
-        id = [parent.inner_widget.dataset.index];
+        index = [parent.inner_widget.dataset.index];
+        id = [parent.inner_widget.dataset.ids[0]];
         //console.log(self.$(".oe_form_field_date").text());
         model_invoice = new instance.web.Model('account.invoice');
         result_invoice = model_invoice.call('dic_invoice', [id]);
