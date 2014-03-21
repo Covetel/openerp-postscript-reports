4f4c97c005dcdd8dfc37801649496c694c2781aa
Carlos Paredes <cparedes@covetel.com.ve>
2014-03-21 15:01:46 -0430

    Corrigiendo error en el js al traer datos de la factura
    Signed-off-by: Carlos Paredes <cparedes@covetel.com.ve>

--- static/src/js/print_invoices.js
+++ static/src/js/print_invoices.js
@@ -2,7 +2,7 @@ openerp.plan_cuentas_cooperativas = function (instance) {
     instance.web.client_actions.add('printer.action', 'instance.plan_cuentas_cooperativas.action');
     instance.plan_cuentas_cooperativas.action = function (parent, action) {
         index = [parent.inner_widget.dataset.index];
-        id = [parent.inner_widget.dataset.ids[0]];
+        id = [parent.inner_widget.dataset.ids[index]];
         //console.log(self.$(".oe_form_field_date").text());
         model_invoice = new instance.web.Model('account.invoice');
         result_invoice = model_invoice.call('dic_invoice', [id]);
