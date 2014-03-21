f5c59907876d5703bac3a0035c1cd79c5cd85d33
Carlos Paredes <cparedes@covetel.com.ve>
2014-03-21 15:03:37 -0430

    Añadiendo codigo para imprimir la factura en la impresora fiscal haciendo uso de covetel-pos
    Signed-off-by: Carlos Paredes <cparedes@covetel.com.ve>

--- static/src/js/print_invoices.js
+++ static/src/js/print_invoices.js
@@ -1,3 +1,19 @@
+function impresora_fiscal(line1,line2){
+    try{
+        if (window.XMLHttpRequest)
+          {// code for IE7+, Firefox, Chrome, Opera, Safari
+          xmlhttp=new XMLHttpRequest();
+          }
+        else
+          {// code for IE6, IE5
+          xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+          }
+          xmlhttp.open("GET","http://127.0.0.1:8200/"+String(line1)+"___"+String(line2),false);
+          xmlhttp.send();
+        }catch(err){
+    }
+}
+
 openerp.plan_cuentas_cooperativas = function (instance) {
     instance.web.client_actions.add('printer.action', 'instance.plan_cuentas_cooperativas.action');
     instance.plan_cuentas_cooperativas.action = function (parent, action) {
@@ -8,8 +24,18 @@ openerp.plan_cuentas_cooperativas = function (instance) {
         result_invoice = model_invoice.call('dic_invoice', [id]);
         result_invoice.done(function (records) {
             invoice = records;
-            console.log(invoice);
-        });    
+            impresora_fiscal('ABRIR1',"ABRIR1");
+            impresora_fiscal('ESCRIBIR',invoice.invoice_number);
+            impresora_fiscal('ESCRIBIR',invoice.date_invoice);
+            impresora_fiscal('ESCRIBIR',invoice.partner_name);
+            for (var i=0; i< invoice.invoice_lines.length;i++) {
+                impresora_fiscal('PRODUCTO',invoice.invoice_lines[i].invoice_line_quantity+"___"+invoice.invoice_lines[i].invoice_line_name+"___"+invoice.invoice_lines[i].invoice_line_price_unit+"___"+invoice.invoice_lines[i].invoice_line_price_subtotal+"_");
+            }
+            impresora_fiscal("SUBTOTAL",invoice.invoice_amount_untaxed);
+            impresora_fiscal("SUBTOTAL",invoice.invoice_amount_tax);
+            impresora_fiscal("SUBTOTAL",invoice.invoice_amount_total);
+            impresora_fiscal("CERRAR1","CERRAR1");
+        });
         //model_invoice.query().filter([['id', '=', id]]).all().done(function (records) {
         //    console.log(_(records));
         //});
